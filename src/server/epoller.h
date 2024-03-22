66ec156035b011eaf137544c7dc89e8d5079c23b
nepleo <nepleoli@foxmail.com>
2024-03-22 14:02:17 +0800

    format style

--- src/server/epoller.h
+++ src/server/epoller.h
@@ -5,20 +5,21 @@
 #include <fcntl.h>
 #include <sys/epoll.h>
 #include <unistd.h>
+
 #include <vector>
 
 class Epoller {
 public:
-	explicit Epoller(int max_event = 1024);
-	~Epoller();
-	bool AddFd(int fd, uint32_t events_type);
-	bool ModFd(int fd, uint32_t events_type);
-	bool DelFd(int fd);
-	int Wait(int timeout_ms = -1);
-	int GetEventFd(size_t i) const;
-	uint32_t GetEvents(size_t i) const;
+  explicit Epoller(int max_event = 1024);
+  ~Epoller();
+  bool AddFd(int fd, uint32_t events_type);
+  bool ModFd(int fd, uint32_t events_type);
+  bool DelFd(int fd);
+  int Wait(int timeout_ms = -1);
+  int GetEventFd(size_t i) const;
+  uint32_t GetEvents(size_t i) const;
 
 private:
-	int m_epollfd;
-	std::vector<struct epoll_event> m_events;
+  int m_epollfd;
+  std::vector<struct epoll_event> m_events;
 };
